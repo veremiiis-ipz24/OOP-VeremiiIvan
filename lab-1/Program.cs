f3a9b5a10f58d4834831e23730a854690e6fa669
Веремій Іван <veremii.i.s_ipz24@rcit.ukr.education>
2025-09-05 12:57:34 +0300

    TEST

---
+++ lab-1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OOP_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure f = new Figure(30.3);
+            Console.WriteLine(f.GetFigure());
+
+            f.Area = 50.8;
+            Console.WriteLine(f.GetFigure());
+        }
+    }
+}
